bee34d0f7436b551e7e6281c4a24459138fb8c93
Arturo Alicea <arturo.alicea@outlook.com>
2020-11-28 21:15:30 -0500

    Initial commit for the profile in vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="background">
     <router-view />
   </div>
 </template>
@@ -17,16 +13,11 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html {
+  background-image: url("https://wmad.cs.vt.edu/images/code_background.png");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="card">
+    <img src="@/assets/profile.jpeg" class="profile" />
+    <h1>Arturo Alicea</h1>
+    <p>Fullstack Software Developer</p>
+    <a target="_blank" rel="noopener noreferrer" href="./ArturoAlicea-Resume.pdf">
+      <img src="@/assets/resume.png" class="resume" />
+    </a>
+    <Contact />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Contact from "@/components/Contact.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Contact
   }
 };
 </script>
+
+<style scoped>
+.card {
+  background-color: aliceblue;
+  border-radius: 25px;
+  margin: 0 auto;
+  font-family: "BebasNeue";
+  padding: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.profile {
+  height: auto;
+  width: 35%;
+  border-radius: 50%;
+}
+
+h1 {
+  font-size: 3vw;
+  font-weight: bold;
+  letter-spacing: 5px;
+  line-height: 10px;
+}
+
+p {
+  font-size: 1.5vw;
+}
+
+.resume {
+  height: auto;
+  width: 12%;
+  float: right;
+  margin: -20px -20px;
+}
+</style>
